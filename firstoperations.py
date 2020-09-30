fb1535a94fc23ec1e3a72cf23a65f8cb70af8a2c
Thijn <thijnglas@gmail.com>
2020-09-30 11:57:00 +0200

    bestanden toegevoegd

---
+++ firstoperations.py
@@ -0,0 +1,20 @@
+heelgetal=133
+PuntGetal=12.5
+eengetal=4
+tiental= 20
+duizendgetal= 2000
+
+PuntGetal *= 4
+heelgetal += 5
+eengetal -= 2
+tiental /= 2
+duizendgetal= 2000 % 18
+heelgetal = 133+5
+duizendgetal %= 18
+
+
+print (PuntGetal)
+print (heelgetal)
+print (eengetal)
+print (tiental)
+print (duizendgetal)
---
+++ player statistics.py
@@ -0,0 +1,21 @@
+naam = "Thijn Glas "
+leeftijd = 16
+gewicht = "80 kilogram"
+lengte = "1,75 meter"
+snelheid = "max 24.8 km/uur"
+points = "54.5"
+ready = True
+training = "done"
+lives = 3
+jumpforce = 20
+
+print ("naam: " + naam)
+print ("leeftijd: " + str(leeftijd))
+print ("gewicht: " + gewicht)
+print ("lengte: " + lengte)
+print ("maximale snelheid: " + snelheid)
+print ("punten: " + points)
+print ("ready: " + str(ready))
+print ("training: " + training)
+print ("levens: " + str(lives))
+print ("jumpforce: " + str(jumpforce))
